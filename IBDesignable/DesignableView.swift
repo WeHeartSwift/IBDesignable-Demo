858969908f4b07367cd47b582aa9ac0b1acd24b8
Andrei Puni <puni.andrei23@gmail.com>
2014-06-06 23:58:05 +0300

    https://github.com/WeHeartSwift/IBDesignable-Demo.git code

---
+++ IBDesignable/DesignableView.swift
@@ -0,0 +1,33 @@
+//
+//  DesignableView.swift
+//  IBDesignable
+//
+//  Created by Andrei Puni on 06/06/14.
+//  Copyright (c) 2014 Andrei Puni. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableView: UIView {
+    @IBInspectable var borderColor: UIColor = UIColor.clearColor() {
+    didSet {
+        layer.borderColor = borderColor.CGColor
+    }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+    didSet {
+        layer.borderWidth = borderWidth
+    }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+    didSet {
+        layer.cornerRadius = cornerRadius
+    }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ IBDesignableTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBDesignableTest
+//
+//  Created by Andrei Puni on 06/06/14.
+//  Copyright (c) 2014 Andrei Puni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
