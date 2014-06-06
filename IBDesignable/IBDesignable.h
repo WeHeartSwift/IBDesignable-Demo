23d84f7496a95ebeefac131c383c073586268f6e
Andrei Puni <puni.andrei23@gmail.com>
2014-06-06 17:36:45 +0300

    Initial Commit

---
+++ IBDesignable/IBDesignable.h
@@ -0,0 +1,19 @@
+//
+//  IBDesignable.h
+//  IBDesignable
+//
+//  Created by Andrei Puni on 06/06/14.
+//  Copyright (c) 2014 Andrei Puni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBDesignable.
+FOUNDATION_EXPORT double IBDesignableVersionNumber;
+
+//! Project version string for IBDesignable.
+FOUNDATION_EXPORT const unsigned char IBDesignableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBDesignable/PublicHeader.h>
+
+
